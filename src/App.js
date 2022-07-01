583f784e820fdcc9a1ac9d1b292bccfa3a1f061e
pat123 <phollakrit@hotmail.com>
2022-07-02 00:11:13 +0700

    create style.css and modify App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-/*import logo from './logo.svg';*/
-import './App.css';
-
+/*import logo from './logo.svg';
+import './App.css';*/
+import React from 'react'
 function App() {
-  return (
-    <h3>Hello Word! <br/>สวัสดีชาวโลก</h3>
-  )
-}
+  let colors = ['red', 'green', 'blue', 'yellow']
+  let list = colors.map(c => <li>{c}</li>)      //แทรกค่าแบบ JSX
+  //list = <li>red</li><li>green</li>...
+  return <ul>{list}</ul>
 
+  //หรือเรียกเมธอด map() ใน JSX โดยตรง
+  /*
+  return (
+    <ul>
+      {colors.map(c => <li>{c}</li>)}
+    </ul>
+  )
+  */
+}
 export default App;
