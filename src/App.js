006cc3ea570d19be867f1398317c567c9cf605f2
pat123 <phollakrit@hotmail.com>
2022-07-04 12:42:19 +0700

    add class-components and func-components and update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-/*import logo from './logo.svg';
-import './App.css';*/
 import React from 'react'
-function App() {
-  let colors = ['red', 'green', 'blue', 'yellow']
-  let list = colors.map(c => <li>{c}</li>)      //แทรกค่าแบบ JSX
-  //list = <li>red</li><li>green</li>...
-  return <ul>{list}</ul>
+import { Header, Content, Footer } from './func-components'
+import Calendar from './class-components'
 
-  //หรือเรียกเมธอด map() ใน JSX โดยตรง
-  /*
-  return (
-    <ul>
-      {colors.map(c => <li>{c}</li>)}
-    </ul>
-  )
-  */
+function App() {
+	return (
+		<>
+			<Header/>
+			<p><center><Calendar/></center></p>
+			<Content/>
+			<Footer/>
+	  	</>
+	)
+	
 }
-export default App;
+export default App
