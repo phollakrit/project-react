8398f04299db17f6f9443e38c07c74877289705f
pat123 <phollakrit@hotmail.com>
2022-07-11 17:29:48 +0700

    modify class-components.js and add calculator.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Banner from './banner'
+import {Calculator2}  from './calculator'
 
 export default function App() {
-	return <Banner/>	
+	return <Calculator2/>	
 }
+
